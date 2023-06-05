332bf91f98b17fb3592be76b4fd23c5aa1526a79
Rothin Roy <mr.rothinroy@gmail.com>
2023-06-05 18:23:31 +0600

    meal db commit

---
+++ js/rendom.js
@@ -0,0 +1,17 @@
+const loadUser = () => {
+    fetch('https://randomuser.me/api/?gender=female')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+}
+
+const displayUser = user => {
+    const genderTag = document.getElementById('gender')
+    genderTag.innerHTML = user.results[0].gender;
+
+    const name = user.results[0].name.first + ' ' + user.results[0].name.last + ' ' + user.results[0].name.title;
+    document.getElementById('name').innerHTML = name;
+
+    console.log(user.results[0].name);
+}
+
+loadUser();
